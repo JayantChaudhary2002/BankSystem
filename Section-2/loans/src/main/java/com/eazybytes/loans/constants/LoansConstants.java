0ef3f029b36f2e120d109ca61076d4a2523ce250
Jayant Chaudhary <jayantchaudhary2002@gmail.com>
2026-07-29 17:00:13 +0530

    Under work loans microservice

---
+++ Section-2/loans/src/main/java/com/eazybytes/loans/constants/LoansConstants.java
@@ -0,0 +1,18 @@
+package com.eazybytes.loans.constants;
+
+public final class LoansConstants {
+
+    private LoansConstants(){}
+
+    public static final String HOME_LOAN = "Home Loan";
+    public static final int New_LOAN_LIMIT = 1_00_000;
+    public static final String STATUS_201 = "201";
+    public static final String MESSAGE_201 = "Loan Created successfully";
+    public static final String STATUS_200 = "200";
+    public static final String MESSAGE_200 = "Request processed successfully";
+    public static final String STATUS_417 = "417";
+    public static final String MESSAGE_417_UPDATE = "Update Operation Failed. Please try again or contact Dev team";
+    public static final String MESSAGE_417_DELETE = "Delete Operation Failed. Please try again or contact Dev team";
+//    public static final String STATUS_500 = "500";
+//    public static final String MESSAGE_500 = "An error occurred. Please try again or contact Dev team";
+}
